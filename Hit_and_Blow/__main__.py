cc24f61e6bd87da1b0202c3c664f86c1ddd5c251
minomushi <wngx5451@yahoo.co.jp>
2022-03-08 04:39:43 -0500

    完成

--- Hit_and_Blow/__main__.py
+++ Hit_and_Blow/__main__.py
@@ -2,38 +2,40 @@ import random
 import sys
 import argparse
 
-def generate(n):
+def generate(n, zero):
     num = str(random.randint(10**(n-1), 10**n))
-    while not appropriate(num):
+    while not appropriate(num, zero):
         num = str(random.randint(10**(n-1), 10**n))
     return num
 
-def appropriate(num):
-    return all(map(lambda s: num.count(s) == 1, num))
+def appropriate(num, zero):
+    return all(map(lambda s: num.count(s) == 1 and (zero or s != "0"), num))
 
 def argument():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--digits", type=lambda n: ((1<=n) and int(n)) or False, metavar="[1~...]", default=3, help="桁数")
+    parser.add_argument("-d", "--digits", type=lambda n: ((1<=int(n)) and int(n)) or False, metavar="[1~...]", default=3, help="桁数")
     parser.add_argument("-p", "--duplicate", action="store_true", help="正解となる数字の重複を許可(正解に121, 111等の数字も含まれるようになる)")
+    parser.add_argument("-i", "--include-zero", action="store_true", help="正解となる数字に0を含める")
     return parser.parse_args()
 
 def main():
     args = argument()
-    answer = generate(args.digits)
+    answer = generate(args.digits, args.include_zero)
     count = 0
     while True:
         count += 1
         while True:
-            print("あなたの予想を書いてください({}桁)> ".format(args.digits), end="")
+            print("\nあなたの予想を書いてください({}桁)> ".format(args.digits), end="")
+            sys.stdout.flush()
             try:
-                num = int(sys.stdin.readline())
+                num = str(int(sys.stdin.readline()))
                 if len(num) != args.digits:
                     raise ValueError()
                 break
             except ValueError:
-                pass
+                print("\033[1F\033[0J", end="")
         if num == answer:
-            print("正解です！！\n答え: {}\n{}回で当てました！".format(answer, count))
+            print("\n正解です！！\n答え: {}\n{}回で当てました！".format(answer, count))
             break
         hit = 0
         blow = 0
@@ -43,6 +45,7 @@ def main():
             elif s in answer:
                 blow += 1
         print("{}ヒット {}ブロー".format(hit, blow))
+        sys.stdout.flush()
 
 if __name__ == "__main__":
     main()
